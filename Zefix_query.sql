ffa513ebf9506db235215f1b5da7c5a53f107bb7
Nick | 512m <93676606+TheDeGen@users.noreply.github.com>
2025-01-09 19:49:30 +0100

    Update Zefix_query.sql
    Removed some stuff, tinkering with data structures

--- Zefix_query.sql
+++ Zefix_query.sql
@@ -10,18 +10,6 @@ WHERE {
   ?company_uri schema:additionalType ?type_id .
   ?type_id schema:name ?company_type .
 
-  FILTER langMatches(lang(?name), "de") .
-  FILTER langMatches(lang(?company_type), "de") .
-  FILTER NOT EXISTS {
-    FILTER (
-      CONTAINS(LCASE(?description), "crypto") || 
-      CONTAINS(LCASE(?description), "krypto") || 
-      CONTAINS(LCASE(?description), "blockchain") || 
-      CONTAINS(LCASE(?description), "dlt") || 
-      CONTAINS(LCASE(?description), "bitcoin") || 
-      CONTAINS(LCASE(?description), "token") || 
-      CONTAINS(LCASE(?description), "nft")
-    )
-  }
+  FILTER (CONTAINS(LCASE(?description), "blockchain")) .
 } 
-LIMIT 50
+LIMIT 10
